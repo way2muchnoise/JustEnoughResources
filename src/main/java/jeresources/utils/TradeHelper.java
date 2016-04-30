28ae9087c8b815c0932719a8a9ad372afcdb26a4
way2muchnoise <jens.baetens@live.be>
2016-04-30 22:10:02 +0200

    add obf name for villager trades

--- src/main/java/jeresources/utils/TradeHelper.java
+++ src/main/java/jeresources/utils/TradeHelper.java
@@ -6,6 +6,6 @@ public class TradeHelper
 {
     public static EntityVillager.ITradeList[][][][] getTrades()
     {
-        return ReflectionHelper.getPrivateValue(EntityVillager.class, null, "DEFAULT_TRADE_LIST_MAP");
+        return ReflectionHelper.getPrivateValue(EntityVillager.class, null, "DEFAULT_TRADE_LIST_MAP", "field_175561_bA");
     }
 }
