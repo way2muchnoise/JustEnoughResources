fb76ae4fe2fa1595a8276b2a55ba5b9f3adcae24
way2muchnoise <jens.baetens@live.be>
2016-06-08 22:40:45 +0200

    JER villager reg doesn't need to be mapped

--- src/main/java/jeresources/registry/VillagerRegistry.java
+++ src/main/java/jeresources/registry/VillagerRegistry.java
@@ -3,13 +3,14 @@ package jeresources.registry;
 import jeresources.entry.VillagerEntry;
 import jeresources.util.VillagersHelper;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class VillagerRegistry
 {
     private static VillagerRegistry instance;
 
-    private Map<String, Map<Integer, VillagerEntry>> villagers;
+    private List<VillagerEntry> villagers;
 
     public static VillagerRegistry getInstance()
     {
@@ -20,33 +21,17 @@ public class VillagerRegistry
 
     private VillagerRegistry()
     {
-        this.villagers = new HashMap<>();
+        this.villagers = new LinkedList<>();
         VillagersHelper.initRegistry(this);
     }
 
     public void addVillagerEntry(VillagerEntry entry)
     {
-        Map<Integer, VillagerEntry> entryMap = this.villagers.get(entry.getName());
-        if (entryMap == null) entryMap = new HashMap<>();
-        entryMap.put(entry.getCareer(), entry);
-        this.villagers.put(entry.getName(), entryMap);
+        this.villagers.add(entry);
     }
 
     public List<VillagerEntry> getVillagers()
     {
-        List<VillagerEntry> list = new ArrayList<>();
-        for (Map<Integer, VillagerEntry> villagerMap : this.villagers.values())
-            list.addAll(villagerMap.values());
-        return list;
-    }
-
-    public Map<Integer, VillagerEntry> getVillagerCareer(String name)
-    {
-        return this.villagers.get(name);
-    }
-
-    public VillagerEntry getVillager(String name, int profession)
-    {
-        return this.villagers.containsKey(name) ? this.villagers.get(name).get(profession) : null;
+        return this.villagers;
     }
 }
