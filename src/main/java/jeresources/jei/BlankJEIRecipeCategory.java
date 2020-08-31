4de50fa0c48b10643f4609c48f471db6ff47480c
way2muchnoise <jens.baetens@live.be>
2020-08-31 18:19:32 +0200

    fixes tooltip hover on ore graph, fixes #211

--- src/main/java/jeresources/jei/BlankJEIRecipeCategory.java
+++ src/main/java/jeresources/jei/BlankJEIRecipeCategory.java
@@ -5,8 +5,10 @@ import mezz.jei.api.gui.drawable.IDrawable;
 import mezz.jei.api.ingredients.IIngredients;
 import mezz.jei.api.recipe.category.IRecipeCategory;
 import mezz.jei.api.recipe.category.extensions.IRecipeCategoryExtension;
+import net.minecraft.util.text.ITextComponent;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 public abstract class BlankJEIRecipeCategory<T extends IRecipeCategoryExtension> implements IRecipeCategory<T> {
     private final IDrawable icon;
@@ -29,5 +31,10 @@ public abstract class BlankJEIRecipeCategory<T extends IRecipeCategoryExtension>
     @Override
     public void draw(T recipe, MatrixStack matrixStack, double mouseX, double mouseY) {
         recipe.drawInfo(getBackground().getWidth(), getBackground().getHeight(), matrixStack, mouseX, mouseY);
+    }
+
+    @Override
+    public List<ITextComponent> getTooltipStrings(T recipe, double mouseX, double mouseY) {
+        return recipe.getTooltipStrings(mouseX, mouseY);
     }
 }
