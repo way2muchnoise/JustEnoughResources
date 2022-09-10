5745f34510fcb2a08fc94ad1a8eecfcfce1eb154
way2muchnoise <jens.baetens@live.be>
2022-09-10 15:10:53 +0200

    allow connecting to servers without JER, implements #328

--- Forge/src/main/java/jeresources/JEResources.java
+++ Forge/src/main/java/jeresources/JEResources.java
@@ -9,17 +9,22 @@ import jeresources.proxy.CommonProxy;
 import jeresources.reference.Reference;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(Reference.ID)
 public class JEResources {
     public static CommonProxy PROXY = DistExecutor.safeRunForDist(() -> ClientProxy::new, () -> CommonProxy::new);
 
     public JEResources() {
+        //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(()-> NetworkConstants.IGNORESERVERONLY, (remote, isServer)-> true));
+
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.COMMON);
 
@@ -27,7 +32,7 @@ public class JEResources {
         Config.instance.loadConfig(Config.COMMON, Services.PLATFORM.getConfigDir().resolve(Reference.ID + ".toml"));
         MinecraftForge.EVENT_BUS.register(Config.COMMON);
         MinecraftForge.EVENT_BUS.register(new ProfileCommand());
-    }
+      }
 
     private void commonSetup(FMLCommonSetupEvent event) {
         JERAPI.init();
