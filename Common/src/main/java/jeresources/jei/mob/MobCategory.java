231697ce08c8901e57be8843f42895cc78b8bd7a
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2023-03-12 17:21:15 +0100

    1.19.2 branch (#385)
    * update to 1.19.3
    
    * Adding Spawn Eggs to Mob Category (#378)
    
    * Adds SpawnEggs to Mob Page
    
    I hope you like this :)
    
    * Adds SpawnEggs to Mob Page
    
    I hope you like this :)
    
    * Fix imports
    
    * Fix imports
    
    * Go again
    
    * Show no spawn egg if null
    
    Shows no item in spawn egg slot, if mob does not have egg
    
    * Changes
    
    Changed to 4 spaces instead of 1 tab per level
    Added MobWrapper: hasSpawnEgg and MobWrapper: getSpawnEgg
    Updated gui texture for mob category instead of using  RenderHelper.drawTexture
    
    * Cleanup
    
    Changed gradle.properties specificationVersion=1.3.0 to specificationVersion=1.3.1
    And cleanup in MobWrapper
    
    * 1.19.2 branch
    
    Added spawn egg slot to 1.192.
    Changed "Experience Dropped" to "Experience". Ender dragon exp was overlapping with spawn egg slot.
    Updated pack_format to match 1.19.2.
    Updated specificationVersion to 1.2.2
    
    ---------
    
    Co-authored-by: way2muchnoise <jens.baetens@live.be>

--- Common/src/main/java/jeresources/jei/mob/MobCategory.java
+++ Common/src/main/java/jeresources/jei/mob/MobCategory.java
@@ -60,5 +60,8 @@ public class MobCategory extends BlankJEIRecipeCategory<MobWrapper> {
             }
             xOffset += 72 / Settings.ITEMS_PER_ROW;
         }
+        if (recipeWrapper.hasSpawnEgg()) {
+            builder.addSlot(RecipeIngredientRole.CATALYST, 151, 22).addItemStack(recipeWrapper.getSpawnEgg());
+        }
     }
 }
--- Common/src/main/java/jeresources/jei/mob/MobWrapper.java
+++ Common/src/main/java/jeresources/jei/mob/MobWrapper.java
@@ -150,4 +150,12 @@ public class MobWrapper implements IRecipeCategoryExtension, IRecipeSlotTooltipC
         else if (livingEntity instanceof AbstractPiglin) offsetY = -15;
         return offsetY;
     }
+
+    public boolean hasSpawnEgg() {
+        return getSpawnEgg() != null;
+    }
+
+    public ItemStack getSpawnEgg() {
+        return this.mob.getEntity().getPickResult();
+    }
 }
