bbf5c9e3dd3b01fa401aa59ea5fd8aa82421da8a
Hayden Sim <haydenwillsim@gmail.com>
2023-07-14 04:33:07 +1000

    fix 'Wrong filesystem' error by translating PathPath's into their underlying UnionPaths for nested jars

--- Forge/src/main/java/jeresources/forge/ModInfo.java
+++ Forge/src/main/java/jeresources/forge/ModInfo.java
@@ -22,6 +22,18 @@ public class ModInfo implements IModInfo {
 
     @Override
     public List<? extends PackResources> getPackResources() {
-        return List.of(new PathPackResources(modFile.getFile().getFileName(), false, modFile.getFile().getFilePath()));
+        Path source = modFile.getFile().getFilePath();
+        // for nested jars, the file path is actually a PathPath, it's fileSystem is a
+        // PathFileSystem, but the source.fileSystem.target is a UnionPath pointing to
+        // a UnionFileSystem which is what forge uses, which is what we want, so that
+        // it matches internal registers, without this we'll get 'Wrong filesystem'
+        // errors even though they're the same file pointing to the same thing
+        //
+        // tldr: if it's a PathFileSystem, make it a UnionFileSystem
+        if (PathPath.class.isAssignableFrom(source.getClass())) {
+            PathFileSystem pfs = ((PathPath)source).getFileSystem();
+            source = pfs.getTarget();
+        }
+        return List.of(new PathPackResources(modFile.getFile().getFileName(), source));
     }
 }
