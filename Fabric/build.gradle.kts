a3cfadb7cf52ce5b8fbac187086b47b275beb1b4
way2muchnoise <jens.baetens@live.be>
2026-07-22 17:32:19 +0200

    add environment to CF upload

--- Fabric/build.gradle.kts
+++ Fabric/build.gradle.kts
@@ -2,7 +2,7 @@ import net.darkhax.curseforgegradle.TaskPublishCurseForge
 import net.darkhax.curseforgegradle.Constants as CFG_Constants
 
 plugins {
-	id("net.darkhax.curseforgegradle") version("1.1.18")
+	id("net.darkhax.curseforgegradle") version("1.1.27")
 	id("com.modrinth.minotaur") version("2.+")
 }
 
@@ -87,6 +87,7 @@ tasks.register<TaskPublishCurseForge>("publishCurseForge") {
 	mainFile.addModLoader("Fabric")
 	mainFile.addJavaVersion("Java $modJavaVersion")
 	mainFile.addGameVersion(minecraftVersion)
+	mainFile.addEnvironment("Client")
 	//TODO: Figure out how to upload correct files
 //	mainFile.withAdditionalFile(apiJar.get())
 //	mainFile.withAdditionalFile(tasks.remapSourcesJar.get())
